bcda0e38d1215009127dc33b6d229b204a6ccc82
Rajesh Kannan <rrajeshkannan@gmail.com>
2020-12-30 08:08:19 +0530

    html css bootstrap landing page website

--- bootstrap/Demo005/js/main.js
+++ bootstrap/Demo005/js/main.js
@@ -1,3 +1,12 @@
+$('.navbar a').click(function () {
+  $('body,html').animate(
+    {
+      scrollTop: $('#' + $(this).data('value')).offset().top,
+    },
+    50
+  );
+});
+
 $(document).ready(function () {
   $('.header').height($(window).height());
 });
